1ee0374839aae6832062d6608d20271f34d5a577
{IamGaurav001} <{hellogauravdubey@gmail.com}>
2025-06-07 23:22:04 +0530

    Missing Number

---
+++ Array/missingNumber.java
@@ -0,0 +1,18 @@
+public class missingNumber {
+    public static void main(String[] args) {
+        int[] nums = {3, 0, 1};
+        System.out.println(missingNumber(nums));
+    }
+
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+        int expectedSum = n * (n + 1) / 2; // Sum of first n natural numbers
+        int actualSum = 0;
+
+        for (int num : nums) {
+            actualSum += num;
+        }
+
+        return expectedSum - actualSum; // The missing number
+    }
+}
